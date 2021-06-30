f88b1baf10a7c3a92d3b6232cb2a6cab07587dcf
Chad McElligott <chad.mcelligott@gmail.com>
2021-06-29 23:03:37 -0500

    Initial Commit

--- index.js
+++ index.js
@@ -1,21 +1,73 @@
+'use strict';
 const core = require('@actions/core');
-const wait = require('./wait');
+const get = require('lodash.get');
+const {
+  S3Client,
+  ListObjectVersionsCommand,
+  DeleteObjectsCommand,
+} = require('@aws-sdk/client-s3');
 
-
-// most @actions toolkit packages have async methods
 async function run() {
-  try {
-    const ms = core.getInput('milliseconds');
-    core.info(`Waiting ${ms} milliseconds ...`);
+  const Bucket = core.getInput('bucket', {
+    required: true,
+    trimWhitespace: true,
+  });
+  const Prefix = core.getInput('prefix', { trimWhitespace: true });
+  const allowEmptyPrefix =
+    core.getInput('allowEmptyPrefix', {
+      trimWhitespace: true,
+    }) === 'true';
 
-    core.debug((new Date()).toTimeString()); // debug is only output if you set the secret `ACTIONS_RUNNER_DEBUG` to true
-    await wait(parseInt(ms));
-    core.info((new Date()).toTimeString());
-
-    core.setOutput('time', new Date().toTimeString());
-  } catch (error) {
-    core.setFailed(error.message);
+  if (Prefix === '' && !allowEmptyPrefix) {
+    throw new Error('allowEmptyPrefix is false, but Prefix is empty');
   }
+
+  const s3client = new S3Client({
+    /** pulls configuration from environment */
+    apiVersion: '2006-03-01',
+  });
+
+  let versions = await s3client.send(
+    new ListObjectVersionsCommand({ Bucket, Prefix })
+  );
+
+  let deletedCount = 0;
+  while (get(versions, 'Versions.length', 0) > 0) {
+    const result = await s3client.send(
+      new DeleteObjectsCommand({
+        Bucket,
+        Delete: {
+          Objects: versions.Versions.map((v) => ({
+            Key: v.Key,
+            VersionId: v.VersionId,
+          })),
+        },
+      })
+    );
+
+    if (get(result, 'Errors.length', 0) > 0) {
+      throw new Error(result.Errors.map((e) => e.Message).join('; '));
+    }
+
+    deletedCount += result.Deleted.length;
+
+    versions = versions.IsTruncated
+      ? await s3client.send(
+          new ListObjectVersionsCommand({
+            Bucket,
+            Prefix,
+            KeyMarker: versions.NextKeyMarker,
+            VersionIdMarker: versions.NextVersionIdMarker,
+          })
+        )
+      : null;
+  }
+
+  core.info(`Successfully deleted ${deletedCount} objects.`);
 }
 
-run();
+run().catch((err) => {
+  process.nextTick(() => {
+    core.setFailed(err.message);
+  });
+});
